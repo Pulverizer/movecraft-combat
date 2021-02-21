b4d7e273d35f58a63c30df2195fe903507eb60dc
Pulverizer <Pulverizer@users.noreply.github.com>
2021-02-21 17:28:33 +0000

    Initial commit

---
+++ src/main/java/io/github/pulverizer/movecraft_combat/MovecraftCombat.java
@@ -0,0 +1,24 @@
+package io.github.pulverizer.movecraft_combat;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "movecraft-combat",
+        name = "Movecraft Combat",
+        description = "Combat addon for Movecraft for Sponge",
+        version = "1.0-SNAPSHOT",
+        url = "https://github.com/Pulverizer/Movecraft-for-Sponge",
+        authors = {"BernardisGood", "https://github.com/Pulverizer/Movecraft-for-Sponge/graphs/contributors"})
+
+public class MovecraftCombat {
+
+    @Inject private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+    }
+}
